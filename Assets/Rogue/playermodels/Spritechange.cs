1d0993e1c6fac5d61c090256912d207e1b81fa24
Ev1lMan <1ev1lman98@gmail.com>
2017-05-27 19:57:31 +0400

    Test

--- Assets/Rogue/playermodels/Spritechange.cs
+++ Assets/Rogue/playermodels/Spritechange.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spritechange : MonoBehaviour 
-{
-	public Sprite Clown= new Sprite[3]
-	void Change1()
-	{
+public class Spritechange : MonoBehaviour {
+
+	public Sprite[] Clown= new Sprite[4]; 
+
+	void Update()  {
 		if (Input.GetKey (KeyCode.W)) {
-			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_up");
+			print (gameObject);
+			gameObject.GetComponent<SpriteRenderer> ().sprite = Clown [1];
 		}
 		if (Input.GetKey (KeyCode.A)) {
-			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_left");
+			//gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_left");
 		}
 		if (Input.GetKey (KeyCode.S)) {
-			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_down");
+			//gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_down");
 		}
 		if (Input.GetKey (KeyCode.D)) {
-			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_right");
+			//gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("clown_right");
 		}
 	}
 }
