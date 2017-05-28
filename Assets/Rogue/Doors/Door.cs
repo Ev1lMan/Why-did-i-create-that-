aca1789e9e94313f60f7417072e076311c763fde
Ev1lMan <1ev1lman98@gmail.com>
2017-05-28 15:05:51 +0400

    Do you eve read the history?
    Well, shitty door with shitty code. It just asks me to be rewritten

--- Assets/Rogue/Doors/Door.cs
+++ Assets/Rogue/Doors/Door.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 //Я ТУТ ЕБАЛСЯ ЧАСА 2. ЕЩЁ ЕБУСЬ, КАК ГУСЬ БЛЯТЬ. ГДЕЕЕ МОООЯЯЯ МНОГА ПОТОЧНОСТЬ. СУКИИИ.
 //Хотя всё правильно, любая вызванная функция должна дать выхлоп перед продолжением главного цикла.
 //БЛЯТЬ, АААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААА ЯЯЯЯЯ ХООЧУ КРАСИИИВООО ЕБОШИТЬ, НЕ ХОЧУ ЗАПИХИВАТЬ В КЛАВНЫЙ ЦЫЫЫЫКЛЛЛ СУУУКИИИИ
+//Новая технология поъехала - корутина, но я как-то не оч. СУКА ЗАЕБАЛСЯ Я
 public class Door : MonoBehaviour {
 	public GameObject DoorConsole; //Не важно блять
 	public bool IsOpen;
+	public bool toOpen;
 	public int OpenSpeed; //Ну тут кароч кадыров в секунду ...................
 	public Sprite[] DoorSq = new Sprite[10]; //Удобства не завезли
 	private Sprite _currentSprite;
-	IEnumerator OpenProc(){
+	private float _time;
+	private int _curr;
+/*	IEnumerator OpenProc(){
 		print ("haha");
 		bool _done = false;
 		float _time = 0;
@@ -32,10 +36,14 @@ public class Door : MonoBehaviour {
 			}
 		}
 		yield return null;
-	}
+	}*/
 		
 	// Use this for initialization
 	void Start () {
+		_time = 0;
+		_curr = 3;
+		
+		toOpen = false;
 		if (IsOpen) {
 			_currentSprite = DoorSq [9];
 		}else{
@@ -46,8 +54,34 @@ public class Door : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.R)){
-			StartCoroutine ("OpenProc");
+		if (Input.GetKeyDown (KeyCode.R)) {
+			//StartCoroutine ("OpenProc");
+			toOpen = true;
+		}
+		//ГОООООСПАААДЕЕЕЕ, КАКАЯ УБОГАЯ РЕАЛИЦАЗИЦАЯ, ПОЖАЛУЙСТА, ПЕРЕПИШИТЕ ЭТО КТО-НИБУДЬ В НОРМАЛЬНУЮ ФОРМУ, ЧТОБ ЭТО БЫЛА ФУНКЦИЯ, А НЕ ХУЙНЯ БЛЯТЬ
+		//bool _done;
+		//float _time;
+		//int _curr;
+		//ЛУЧШЕ НЕ СМОТРЕТЬ СЮДА, ЭТО ПРОСТО ЕБАНЫЙ ПОЗОР БЛЯТЬ! А ЧТО Я МОГУ СДЕЛАТЬ? Я БЛЯТЬ НЕ МОГУ С КОРУТИНАМИ РАЗОБРАТЬСЯ, ХОТЬ БЛЯТЬ ОТДЕЛЬНЫЙ ПОТОМ ЗАПУСКАЙ СУКА
+		if(toOpen == true){
+			_time += Time.deltaTime;
+			//_curr = 3;
+			this.GetComponent<SpriteRenderer> ().sprite = DoorSq[_curr];
+			if (_time >= 0.5f) {
+				_curr += 1;
+				_time = 0;
+			}
+			if (_curr >= 8) {
+				toOpen = false;
+				IsOpen = true;
+			}
+
+
+
+
+
+
+
 		}
 	}
-	}
+}
