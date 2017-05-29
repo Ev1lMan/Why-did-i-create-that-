adfed769c738ca924c712b52656aa98b04f19094
Ev1lMan <1ev1lman98@gmail.com>
2017-05-29 22:17:38 +0400

    Test
    Changed the way player moves

--- Assets/Rogue/Actor.cs
+++ Assets/Rogue/Actor.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class Actor : MonoBehaviour {
 	public string Description; //Ну тут кароч чо - опять ооп, каждый будет генерировать своё описание и позже записывать это значение для своего класса
 	public int Move(int Dir, int Speed){	//Движения, ЭТА ЖИ БЛЯТЬ ООП, ДРУГИМ МОБАМ ВПИСЫВАТЬ ЭТО НЕ НАДО УЖЕЕЕЕЕ ЕЕЕЕЕЕААААА
+		this.GetComponent<Rigidbody2D>().freezeRotation = true;
 		if (Dir == 2) {
-			this.transform.Translate (Vector2.up * Speed * Time.deltaTime);
+			//this.transform.Translate (Vector2.up * Speed * Time.deltaTime);
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.up*10);
 		}
 		if (Dir == 4) {
-			this.transform.Translate (Vector2.left * Speed * Time.deltaTime);
+			//this.transform.Translate (Vector2.left * Speed * Time.deltaTime);
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.left*10);
 		}
 		if (Dir == 6) {
-			this.transform.Translate (Vector2.right * Speed * Time.deltaTime);
+			//this.transform.Translate (Vector2.right * Speed * Time.deltaTime);
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.right*10);
 		}
 		if (Dir == 8) {
-			this.transform.Translate (Vector2.down * Speed * Time.deltaTime);
+			//this.transform.Translate (Vector2.down * Speed * Time.deltaTime);
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.down*10);
 		}
 		return 1;
 	}
